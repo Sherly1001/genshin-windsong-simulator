c731238453246cd597160e3325ba2e6f68b5e462
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-03-08 22:20:21 +0700

    chore: settings

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,12 @@
 export class Config {
   cookies: any
   instrument: string
+  instruments: string[]
 
   constructor(cookies: any, ins: string | null) {
     this.cookies = cookies
     this.instrument = ins ?? 'lyre'
+    this.instruments = ['lyre', 'zither', 'piano']
   }
 
   setIns(ins: string) {
