d871c8350987fdb2267fe38fd0e91fab28319e4a
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-03-08 22:19:45 +0700

    feat: settings

--- src/comps/Settings.vue
+++ src/comps/Settings.vue
@@ -1,4 +1,15 @@
-<template></template>
+<template>
+  <div class="inst">
+    <div
+      v-for="ins of config.instruments"
+      :key="ins"
+      :class="`ins ${ins == config.instrument ? 'selected' : ''}`"
+      @click="config.setIns(ins)"
+    >
+      <img :src="`/imgs/instruments/${ins}.png`" />
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import { Config } from '../utils/config'
@@ -12,3 +23,26 @@ export default {
   },
 }
 </script>
+
+<style>
+.inst {
+  position: absolute;
+  top: 2vh;
+  right: 2vw;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 0.3em;
+}
+
+.ins {
+  width: 2.5em;
+  height: 2.5em;
+  cursor: pointer;
+  padding: 0.2em;
+  border-radius: 0.4em;
+}
+
+.ins.selected {
+  background: lightblue;
+}
+</style>
