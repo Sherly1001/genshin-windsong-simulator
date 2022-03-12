7f3456b400765a1be816140a7eb355371d01190d
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-03-12 13:42:47 +0700

    feat: note pressed

--- src/comps/Note.vue
+++ src/comps/Note.vue
@@ -34,18 +34,48 @@ export default {
   methods: {
     play(e: KeyboardEvent | MouseEvent) {
       if (e instanceof KeyboardEvent && e.repeat) return
+      this.note.classList.add('pressed')
       this.audio.currentTime = 0
       this.audio.play()
     },
-    release(_e: KeyboardEvent | MouseEvent) {},
+    release(_e: KeyboardEvent | MouseEvent) {
+      this.note.classList.remove('pressed')
+    },
   },
 }
 </script>
 
 <style>
 .note {
+  position: relative;
   cursor: pointer;
   margin: 0;
   padding: 0;
+  width: 12vh;
+  height: 12vh;
+  border-radius: 50%;
+}
+
+.note.pressed {
+  transform: scale(0.9);
+  background: lightblue;
+}
+
+.note.pressed img {
+  opacity: 0.5;
+}
+
+@media only screen and (max-width: 1024px) {
+  .note {
+    width: 14vh;
+    height: 14vh;
+  }
+}
+
+@media only screen and (max-width: 740px) {
+  .note {
+    width: 16vh;
+    height: 16vh;
+  }
 }
 </style>
